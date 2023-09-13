10a7b3bc2af748e590442f1832bae4c0f5f7fc63
makwanachirag427 <128345607+makwanachirag427@users.noreply.github.com>
2023-09-13 14:17:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const inputs = document.querySelectorAll(".controls input");
+
+function handleChange() {
+  const suffix = this.dataset.sizing || "";
+  console.log(suffix);
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+}
+
+inputs.forEach((input) => input.addEventListener("change", handleChange));
